ce47dca7d5670959063f0358d5e8356e8345cbcf
Markus Hofbauer <markus.hofbauer@tum.de>
2020-11-03 18:54:58 +0100

    apply code format

--- utils/misc/misc.py
+++ utils/misc/misc.py
@@ -6,6 +6,7 @@ import random
 def readCSV(path):
     return pd.read_csv(path)
 
+
 # get random gender distribution
 def getRandomDistribution(df, N):
     idx_list = [None] * N
@@ -13,46 +14,49 @@ def getRandomDistribution(df, N):
         idx_list[i] = random.choice(list(df.index))
     return idx_list
 
+
 # get requested gender distribution
 def getGenderDistribution(df, ratio, N):
     # Get female idxs
-    idx_female = df.index[df['Gender'] == 'female']
-    #females = df.iloc[idx_female]
+    idx_female = df.index[df["Gender"] == "female"]
+    # females = df.iloc[idx_female]
     # Get male idxs
-    idx_male = df.index[df['Gender'] == 'male']
-    #males = df.iloc[idx_male]
-    
+    idx_male = df.index[df["Gender"] == "male"]
+    # males = df.iloc[idx_male]
+
     female_num = int(ratio * N)
     male_num = N - female_num
-    
+
     idx_list = random.choices(idx_female, k=female_num)
     idx_list = idx_list + random.choices(idx_male, k=male_num)
-        
-    #idx_list = random.shuffle(idx_list)
+
+    # idx_list = random.shuffle(idx_list)
     return idx_list
+
 
 # get female lists
 def getFemaleList(df, N):
     idx_list = [None] * N
     # Get female idxs
-    idx_female = df.index[df['Gender'] == 'female']
-    
+    idx_female = df.index[df["Gender"] == "female"]
+
     for i in np.arange(N):
-        #print(np.mod(i, len(df)))
+        # print(np.mod(i, len(df)))
         idx_list[i] = df.index[idx_female[np.mod(i, len(idx_female))]]
-        
-    #idx_list = random.shuffle(idx_list)
+
+    # idx_list = random.shuffle(idx_list)
     return idx_list
+
 
 # get male lists
 def getMaleList(df, N):
     idx_list = [None] * N
     # Get female idxs
-    idx_male = df.index[df['Gender'] == 'male']
-    
+    idx_male = df.index[df["Gender"] == "male"]
+
     for i in np.arange(N):
-        #print(np.mod(i, len(df)))
+        # print(np.mod(i, len(df)))
         idx_list[i] = df.index[idx_male[np.mod(i, len(idx_male))]]
-        
-    #idx_list = random.shuffle(idx_list)
+
+    # idx_list = random.shuffle(idx_list)
     return idx_list
