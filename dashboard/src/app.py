70fb99688b129627d1e0cb6f97903583d435ad9b
aykutaaykut <aykutaaykut.ai@gmail.com>
2021-02-23 22:50:49 +0300

    implemented all modules except laughter and updated the layout in the dashboard

--- dashboard/src/app.py
+++ dashboard/src/app.py
@@ -49,7 +49,7 @@ app.layout = html.Div(
                     )
                 ),
                 
-                # Panel
+                # Audience Panel
                 html.Table(
                     [
                         html.Tr(
@@ -65,15 +65,15 @@ app.layout = html.Div(
                                         value=10,
                                         placeholder='Number of Person',
                                     ),
-                                    style={'width': '18%'}
-                                ),
-                                html.Td(
-                                    html.H5('Female/Male Ratio'),
                                     style={'width': '15%'}
                                 ),
                                 html.Td(
+                                    html.H5('Female Ratio'),
+                                    style={'width': '10%'}
+                                ),
+                                html.Td(
                                     dcc.Slider(
-                                        id='female-male-slider',
+                                        id='female-slider',
                                         min=0,
                                         max=100,
                                         step=0.5,
@@ -84,8 +84,34 @@ app.layout = html.Div(
                                             100: '100'
                                         }
                                     ),
-                                    style={'width': '18%'}
+                                    style={'width': '25%'}
                                 ),
+                                html.Td(
+                                    # Spawn audio
+                                    html.Button('Spawn Audio', id='spawn-audio'),
+                                    style={'width': '12%'}
+                                ),
+                                html.Td(
+                                    # Start stream button
+                                    html.Button('Start Stream', id='start-stream'),
+                                    style={'width': '12%'}
+                                ),
+                                html.Td(
+                                    # Stop stream button
+                                    html.Button('Stop Stream', id='stop-stream'),
+                                    style={'width': '12%'}
+                                )
+                            ]
+                        )
+                    ],
+                    style={'width': '100%'}
+                ),
+                
+                # Panel
+                html.Table(
+                    [
+                        html.Tr(
+                            [
                                 html.Td(
                                     html.H5('Clapping Intensity'),
                                     style={'width': '15%'}
@@ -103,7 +129,20 @@ app.layout = html.Div(
                                             100: '100'
                                         }
                                     ),
-                                    style={'width': '18%'}
+                                    style={'width': '25%'}
+                                ),
+                                html.Td(
+                                    dcc.Loading(
+                                        id='clapping-loading',
+                                        type='circle',
+                                        children=html.Div(
+                                            dcc.Graph(
+                                                id='claps-fig',
+                                                animate=True
+                                            )
+                                        )
+                                    ),
+                                    style={'width': '60%'}
                                 )
                             ]
                         ),
@@ -126,8 +165,25 @@ app.layout = html.Div(
                                             100: '100'
                                         }
                                     ),
-                                    style={'width': '18%'}
+                                    style={'width': '25%'}
                                 ),
+                                html.Td(
+                                    dcc.Loading(
+                                        id='whistling-loading',
+                                        type='circle',
+                                        children=html.Div(
+                                            dcc.Graph(
+                                                id='whistles-fig',
+                                                animate=True
+                                            )
+                                        )
+                                    ),
+                                    style={'width': '60%'}
+                                )
+                            ]
+                        ),
+                        html.Tr(
+                            [
                                 html.Td(
                                     html.H5('Laughter Intensity'),
                                     style={'width': '15%'}
@@ -145,15 +201,32 @@ app.layout = html.Div(
                                             100: '100'
                                         }
                                     ),
-                                    style={'width': '18%'}
+                                    style={'width': '25%'}
                                 ),
                                 html.Td(
-                                    html.H5('Enthusiasm'),
+                                    dcc.Loading(
+                                        id='laughter-loading',
+                                        type='circle',
+                                        children=html.Div(
+                                            dcc.Graph(
+                                                id='laughters-fig',
+                                                animate=True
+                                            )
+                                        )
+                                    ),
+                                    style={'width': '60%'}
+                                )
+                            ]
+                        ),
+                        html.Tr(
+                            [
+                                html.Td(
+                                    html.H5('Booing Intensity'),
                                     style={'width': '15%'}
                                 ),
                                 html.Td(
                                     dcc.Slider(
-                                        id='enthusiasm',
+                                        id='booing-intensity',
                                         min=0,
                                         max=100,
                                         step=0.5,
@@ -164,24 +237,32 @@ app.layout = html.Div(
                                             100: '100'
                                         }
                                     ),
-                                    style={'width': '18%'}
+                                    style={'width': '25%'}
+                                ),
+                                html.Td(
+                                    dcc.Loading(
+                                        id='booing-loading',
+                                        type='circle',
+                                        children=html.Div(
+                                            dcc.Graph(
+                                                id='boos-fig',
+                                                animate=True
+                                            )
+                                        )
+                                    ),
+                                    style={'width': '60%'}
                                 )
                             ]
                         )
                     ],
                     style={'width': '100%'}
                 ),
-                
-                # Spawn audio
-                html.Button('Spawn Audio', id='spawn-audio'),
-                
-                # Start stream button
-                html.Button('Start Stream', id='start-stream'),
-                
-                # Stop stream button
-                html.Button('Stop Stream', id='stop-stream')
             ]
         ),
+        dcc.Store(id='claps'),
+        dcc.Store(id='whistles'),
+        dcc.Store(id='boos'),
+        dcc.Store(id='laughters'),
         dcc.Store(id='audio'),
         dcc.Store(id='stream-counter', storage_type='session'),
         dcc.Interval(
@@ -194,24 +275,35 @@ app.layout = html.Div(
 
 
 @app.callback(
-    Output('audio', 'data'),
+    [
+        Output('claps', 'data'),
+        Output('whistles', 'data'),
+        Output('laughters', 'data'),
+        Output('boos', 'data'),
+        Output('audio', 'data'),
+    ],
     Input('spawn-audio', 'n_clicks'),
     [
         State('n-person', 'value'),
-        State('female-male-slider', 'value'),
+        State('female-slider', 'value'),
         State('clapping-intensity', 'value'),
         State('whistling-intensity', 'value'),
         State('laughter-intensity', 'value'),
-        State('enthusiasm', 'value')
+        State('booing-intensity', 'value')
     ]
 )
-def update_audio(n_clicks:int, n_person:int, female_to_male_ratio:float, 
+def update_audio(n_clicks:int, n_person:int, female_ratio:float, 
                 clapping_intensity:float, whistling_intensity:float, 
-                laughter_intensity:float, enthusiasm:float):
-    # Spawn claps
-    claps = utils_audio.spawnClaps(n_person, SR, DURATION_IN_SEC)
+                laughter_intensity:float, booing_intensity:float):
+    n_female = n_person * (female_ratio / 100)
+    n_male = n_person - n_female
     
-    # Spawn whistles
+    # Spawn Claps
+    n_person_clapping = int(n_person * clapping_intensity / 100)
+    claps = utils_audio.spawnClaps(n_person_clapping, SR, DURATION_IN_SEC)
+    #####
+    
+    # Spawn Whistles
     root_whistle = utils_audio.spawnWhistles(
         SR, 
         can_radius=200, 
@@ -235,16 +327,76 @@ def update_audio(n_clicks:int, n_person:int, female_to_male_ratio:float,
     whistles = np.zeros_like(claps)
     n_person_whistling = int(n_person * whistling_intensity / 100)
     for _ in range(n_person_whistling):
-        start = int(np.random.uniform(0, DURATION_IN_SEC - 1) * SR)
+        start = int(np.random.uniform(0, DURATION_IN_SEC * SR - root_whistle.shape[0]))
         direction = np.random.randint(0, 2)
         whistle = utils_audio.changePitch(root_whistle, SR, np.random.uniform(-3, 3))
         
         whistles[direction, start:start + len(whistle)] += whistle
         
-    # Combine modules
-    audio = claps + (whistles * 200)
+    whistles *= 200
+    #####
     
-    return audio.T
+    # Spawn Laughters
+    laughters = np.random.randn(*claps.shape) #TODO
+    
+    # Spawn Female Boos
+    root_female_boo, root_female_boo_sr = utils_audio.loadAudio(
+        os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data', 
+            'female_boo.m4a'
+        ),
+        sr=44100
+    )
+    root_female_boo = root_female_boo[118000:-107000] # Manual cropping
+    
+    female_boos = np.zeros_like(claps)
+    n_female_booing = int(n_female * booing_intensity / 100)
+    for _ in range(n_female_booing):
+        start = int(np.random.uniform(0, DURATION_IN_SEC * SR - root_female_boo.shape[0]))
+        direction = np.random.randint(0, 2)
+        boo = utils_audio.changePitch(root_female_boo, root_female_boo_sr, np.random.uniform(-3, 3))
+
+        female_boos[direction, start:start + len(boo)] += boo
+        
+    female_boos *= 4000
+    #####
+    
+    # Spawn Male Boos
+    root_male_boo, root_male_boo_sr = utils_audio.loadAudio(
+        os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'data', 
+            'male_boo.mp3'
+        ),
+        sr=44100
+    )
+    
+    male_boos = np.zeros_like(claps)
+    n_male_booing = int(n_male * booing_intensity / 100)
+    for _ in range(n_male_booing):
+        start = int(np.random.uniform(0, DURATION_IN_SEC * SR - root_male_boo.shape[0]))
+        direction = np.random.randint(0, 2)
+        boo = utils_audio.changePitch(root_male_boo, root_male_boo_sr, np.random.uniform(-3, 3))
+
+        male_boos[direction, start:start + len(boo)] += boo
+        
+    male_boos *= 100
+    #####
+    
+    # Combine Boos
+    boos = female_boos + male_boos
+    
+    # Combine audios
+    audio = (
+        claps
+        + whistles
+        # + laughters
+        + boos
+    )
+    #####
+    
+    return claps.T, whistles.T, laughters.T, boos.T, audio.T
 
 
 @app.callback(
@@ -260,9 +412,9 @@ def update_figure(audio:str, stream_counter:int, start_stream_click:int):
     input_id = ctx.triggered[0]['prop_id'].split('.')[0]
     
     if input_id == 'audio':
-        return plot_soundwave(audio, 0)
+        return plot_soundwave(audio, 0, 'Soundwave')
     elif input_id == 'stream-counter':
-        return plot_soundwave(audio, stream_counter / SR * AUDIO_BLOCKSIZE)
+        return plot_soundwave(audio, stream_counter / SR * AUDIO_BLOCKSIZE, 'Soundwave')
     elif input_id == 'start-stream':
         def callback(outdata, frames, time, status):
             if status:
@@ -280,10 +432,10 @@ def update_figure(audio:str, stream_counter:int, start_stream_click:int):
             else:
                 outdata[:] = data
         
-        fig = plot_soundwave(audio, stream_counter / SR * AUDIO_BLOCKSIZE)
+        fig = plot_soundwave(audio, stream_counter / SR * AUDIO_BLOCKSIZE, 'Soundwave')
         
         q = queue.Queue(maxsize=AUDIO_BLOCKSIZE)
-
+        
         stream = sd.OutputStream(
             samplerate=SR, 
             channels=2, 
@@ -299,7 +451,41 @@ def update_figure(audio:str, stream_counter:int, start_stream_click:int):
                 q.put(data)
                 data = audio[stream_counter:stream_counter + AUDIO_BLOCKSIZE]
                 
-                
+        return 0
+    
+    
+@app.callback(
+    Output('claps-fig', 'figure'),
+    Input('claps', 'data')
+)
+def update_claps_figure(claps:np.ndarray):
+    return plot_soundwave(claps, 0, 'Claps Soundwave')
+
+
+@app.callback(
+    Output('whistles-fig', 'figure'),
+    Input('whistles', 'data')
+)
+def update_whistles_figure(whistles:np.ndarray):
+    return plot_soundwave(whistles, 0, 'Whistles Soundwave')
+
+
+@app.callback(
+    Output('laughters-fig', 'figure'),
+    Input('laughters', 'data')
+)
+def update_laughters_figure(laughters:np.ndarray):
+    return plot_soundwave(laughters, 0, 'Laughters Soundwave')
+
+
+@app.callback(
+    Output('boos-fig', 'figure'),
+    Input('boos', 'data')
+)
+def update_boos_figure(boos:np.ndarray):
+    return plot_soundwave(boos, 0, 'Boos Soundwave')
+
+
 @app.callback(
     Output('stream-counter', 'data'),
     [
@@ -315,10 +501,12 @@ def update_stream_counter(clickData:dict, n_clicks:str):
     return stream_counter
 
 
-def plot_soundwave(audio:np.ndarray, current_x:int) -> go.Figure:
+def plot_soundwave(audio:np.ndarray, current_x:int, title:str) -> go.Figure:
     x = np.array(audio).mean(axis=1)
     x = np.array_split(x, len(x) // SR * AUDIO_BLOCKSIZE)
     x = list(map(lambda x: np.mean(x), x))
+    
+    eps = 1e-15
     
     current_x = int(current_x)
     
@@ -345,17 +533,18 @@ def plot_soundwave(audio:np.ndarray, current_x:int) -> go.Figure:
             )
         )
     
-    fig.add_shape(
-        type='line',
-        x0=current_x, y0=min(x) * (1 - (0.05 * min(x) / abs(min(x)))), 
-        x1=current_x, y1=max(x) * (1 + (0.05 * max(x) / abs(max(x)))),
-        line=dict(
-            width=3
+    if title == 'Soundwave':
+        fig.add_shape(
+            type='line',
+            x0=current_x, y0=min(x) * (1 - (0.05 * min(x) / abs(min(x) + eps))), 
+            x1=current_x, y1=max(x) * (1 + (0.05 * max(x) / abs(max(x) + eps))),
+            line=dict(
+                width=3
+            )
         )
-    )
     
     fig.update_layout(
-        title_text='Soundwave',
+        title_text=title,
         xaxis=dict(
             title_text='Seconds',
             range=[
@@ -367,8 +556,8 @@ def plot_soundwave(audio:np.ndarray, current_x:int) -> go.Figure:
         ),
         yaxis=dict(
             range=[
-                min(x) * (1 - (0.1 * min(x) / abs(min(x)))), 
-                max(x) * (1 + (0.1 * max(x) / abs(max(x))))
+                min(x) * (1 - (0.1 * min(x) / abs(min(x) + eps))), 
+                max(x) * (1 + (0.1 * max(x) / abs(max(x) + eps)))
             ],
             showticklabels=False
         )
